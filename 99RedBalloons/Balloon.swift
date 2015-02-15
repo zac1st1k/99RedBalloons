95fc38e3dfc33a82387aba1e5a0ac652fdb85e05
zac1st1k <zacfirst@gmail.com>
2015-02-15 22:35:41 +1100

    + Balloon struct

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Zac on 5/02/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var number = 0
+    var image = UIImage()
+}
